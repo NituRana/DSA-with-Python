a1659e3a587de19574c41a9637e8a9f36576fd1b
NituRana <niturana0211@gmail.com>
2023-03-03 23:56:34 +0530

    days 5

---
+++ DSA-with-Python/Arrays/Level2_medium/7.remove_duplicates_from_an_array.py
@@ -0,0 +1,25 @@
+'''*******************************************************************************************************
+remove duplicates from an array :
+Write a Python program to remove duplicates from an array.
+
+Example:
+Input array: [3, 7, 8, 6, 3, 9, 15, 17, 7, 2]
+Output array: [3, 7, 8, 6, 9, 15, 17, 2]
+***********************************************************************************************************'''
+def remove_duplicates(arr):
+    unique_arr = []
+    for i in range(0, len(arr)):
+        if arr[i] in unique_arr:
+           pass 
+        else:
+            unique_arr.append(arr[i])
+    return unique_arr
+
+arr = [3, 7, 8, 6, 3, 9, 15, 17, 7, 2]
+print(" This is the array list:- ",arr)
+
+print("The array after remove duplicates is :- ", remove_duplicates(arr))
+
+'''************************************************************************************************************
+Complexcity - n
+*************************************************************************************************************'''
