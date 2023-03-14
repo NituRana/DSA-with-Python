9e137dcdec9f99ab4d4bfb9d5321d0359a232e67
NituRana <niturana0211@gmail.com>
2023-03-14 23:48:09 +0530

    Day 16

---
+++ DSA-with-Python/Strings/Level1_Easy/5.reverse_a_string.py
@@ -0,0 +1,12 @@
+'''*******************************************************************************************************
+reverse a string
+Input : "This is a test string"
+Output : gnirts tset a si sihT
+
+
+***********************************************************************************************************'''
+string = "This is a test string"
+reversed_string = string[::-1]
+print("The reversed string is:", reversed_string)
+ 
+   
