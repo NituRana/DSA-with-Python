71e29cbfe75c39c2aace5bb4d4e242910d067709
NituRana <niturana0211@gmail.com>
2023-04-05 23:29:51 +0530

    day34

---
+++ DSA-with-Python/Stack/2.Implement_a_stack.py
@@ -0,0 +1,31 @@
+'''*******************************************************************************************************
+
+Problem: Implement a stack class that can push, pop, and check if it's empty.
+
+***********************************************************************************************************'''
+
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.stack.pop()
+        else:
+            return None
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+
+s = Stack()
+s.push(1)
+s.push(2)
+s.push(3)
+print(s.pop())
+print(s.pop())
+print(s.is_empty())
