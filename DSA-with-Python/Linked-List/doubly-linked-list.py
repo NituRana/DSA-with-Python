fcaee57eb64305c1fee1da05dbc2e92b9a2c7bf4
NituBfit <niturana0211@gmail.com>
2024-08-10 23:32:29 +0530

    add new node in linked-list

--- DSA-with-Python/Linked-List/doubly-linked-list.py
+++ DSA-with-Python/Linked-List/doubly-linked-list.py
@@ -23,3 +23,20 @@ class Node:
         self.data = data
         self.next = None
         self.prev = None
+        
+# Doubly Linked List class contains methods to manipulate the list
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    # Method to append a new node at the end of the list
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+        new_node.prev = last
