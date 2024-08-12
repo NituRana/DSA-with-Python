9c1bfe5fd231dfb6fd0f0e0b4c0fd58c0a9cfcd5
NituRana <niturana0211@gmail.com>
2024-08-12 18:15:53 +0530

    display the elemnets of the DLL

--- DSA-with-Python/Linked-List/doubly-linked-list.py
+++ DSA-with-Python/Linked-List/doubly-linked-list.py
@@ -5,7 +5,8 @@ A Doubly Linked List (DLL) is a type of linked list where each node contains thr
 Data Field: Stores the actual data.
 Next Pointer: Points to the next node in the list.
 Previous Pointer: Points to the previous node in the list.
-Unlike a singly linked list, where each node points only to the next node, a doubly linked list allows traversal in both directions (forward and backward), thanks to the additional pointer to the previous node.
+Unlike a singly linked list, where each node points only to the next node, a doubly linked list allows 
+traversal in both directions (forward and backward), thanks to the additional pointer to the previous node.
 
 Structure of a Doubly Linked List Node
 In a doubly linked list, each node is typically represented using a structure or class with three fields:
@@ -30,7 +31,7 @@ class DoublyLinkedList:
         self.head = None
 
     # Method to append a new node at the end of the list
-    def append(self, data):
+    def append_in_dll(self, data):
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
@@ -41,12 +42,30 @@ class DoublyLinkedList:
         last.next = new_node
         new_node.prev = last
 
-# Method to insert a new node at the beginning of the list
-def prepend(self, data):
-        new_node = Node(data)
-        if self.head is None:
+    # Method to insert a new node at the beginning of the list
+    def prepend(self, data):
+            new_node = Node(data)
+            if self.head is None:
+                self.head = new_node
+                return
+            self.head.prev = new_node
+            new_node.next = self.head
             self.head = new_node
-            return
-        self.head.prev = new_node
-        new_node.next = self.head
-        self.head = new_node
+
+    # method for the display DLL nodes
+    def display(self):
+            # Initialize an empty list to store node data
+            node_list = []
+            temp = self.head
+            # Traverse the list and collect data from each node
+            while temp:
+                node_list.append(temp.data)
+                temp = temp.next
+
+            print(f"These are the elements of DLL :{node_list}")
+
+
+dll = DoublyLinkedList()
+dll.append_in_dll(5)
+dll.append_in_dll(3)
+dll.display()
