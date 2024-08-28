4529d7400277a9938665310c204bfd2e26a1ca27
NituRana <niturana0211@gmail.com>
2024-08-28 17:48:40 +0530

    revesion

--- DSA-with-Python/Algorithms/Sorting-Algorithm/bubble-sort-algorithm.py
+++ DSA-with-Python/Algorithms/Sorting-Algorithm/bubble-sort-algorithm.py
@@ -19,14 +19,27 @@ Explanation: After sorting we get 1,2,3,4,5
 
 def bubble_sort(arr):
     n = len(arr)
-    for i in range(0, n-1):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                # temp = arr[j+1]
-                # arr[j+1] = arr[j]
-                # arr[j] = temp
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                temp = arr[j +1]
+                arr[j + 1] = arr[j]
+                arr[j] = temp
     return arr
 
 arr = [13,46,24,52,20,9]
-print(f"---- output : {bubble_sort(arr)}")
+print("-------------- Before sorting :", arr)
+print("----- After sorting :", bubble_sort(arr))
+# def bubble_sort(arr):
+#     n = len(arr)
+#     for i in range(0, n-1):
+#         for j in range(0, n-i-1):
+#             if arr[j] > arr[j+1]:
+#                 # temp = arr[j+1]
+#                 # arr[j+1] = arr[j]
+#                 # arr[j] = temp
+#                 arr[j], arr[j+1] = arr[j+1], arr[j]
+#     return arr
+
+# arr = [13,46,24,52,20,9]
+# print(f"---- output : {bubble_sort(arr)}")
