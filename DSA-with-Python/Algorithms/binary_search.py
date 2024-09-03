c846efcac3729fc8944bc3c6814a7c4454facbc4
NituRana <niturana0211@gmail.com>
2024-09-03 16:11:47 +0530

    revivsion of binary search tree

--- DSA-with-Python/Algorithms/binary_search.py
+++ DSA-with-Python/Algorithms/binary_search.py
@@ -1,20 +1,37 @@
-def binary_search(arr, target_element):
+# def binary_search(arr, target_element):
+    # arr_len = len(arr)
+    # min_index = 0
+    # max_index = arr_len - 1
+    # while min_index <= max_index:
+    #     mid = (min_index + max_index) // 2
+    #     if target_element == arr[mid]:
+    #         return mid
+    #     else:
+    #         if target_element > arr[mid]:
+    #             min_index = mid + 1
+    #         else:
+    #             max_index = mid - 1
+    # return -1
+
+def binary_search_revisoin(arr, target_element):
     arr_len = len(arr)
     min_index = 0
-    max_index = arr_len - 1
+    max_index = arr_len -1
     while min_index <= max_index:
         mid = (min_index + max_index) // 2
         if target_element == arr[mid]:
             return mid
+        if target_element < arr[mid]:
+            max_index = mid-1
         else:
-            if target_element > arr[mid]:
-                min_index = mid + 1
-            else:
-                max_index = mid - 1
+            min_index = mid+1
     return -1
+
+
 arr = [3, 4, 6, 7, 9, 12, 16, 17]
 target = 6
-res = binary_search(arr, target)
+# res = binary_search(arr, target)
+res = binary_search_revisoin(arr, target)
 if res == -1:
     print("The target is not present.")
 else:
