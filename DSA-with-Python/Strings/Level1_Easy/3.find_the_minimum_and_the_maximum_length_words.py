0315c94a02775bba41d912d9a8584e7c613e5a64
NituRana <niturana0211@gmail.com>
2024-09-04 14:59:28 +0530

    revision on string

--- DSA-with-Python/Strings/Level1_Easy/3.find_the_minimum_and_the_maximum_length_words.py
+++ DSA-with-Python/Strings/Level1_Easy/3.find_the_minimum_and_the_maximum_length_words.py
@@ -6,7 +6,7 @@ Output : Minimum length word: a
 
 ***********************************************************************************************************'''
 def find_min_max_words(string):
-    words = string.split()
+    words = string.split() #splits the string at every whitespace and returns a list of words.
     min_word = max_word = words[0]
 
     for word in words:
@@ -17,8 +17,19 @@ def find_min_max_words(string):
 
     return min_word, max_word
 
+# revision
+def max_min_word(string):
+    words_list = string.split()
+    min_word = max_word = words_list[0]
+    for word in words_list:
+        if len(min_word) > len(word):
+            min_word = word
+        elif len(max_word) < len(word):
+            max_word = word
+    return min_word, max_word
+
 string = "The quick brown fox jumps over the lazy dog"
-min_word, max_word = find_min_max_words(string)
+min_word, max_word = max_min_word(string)
 print(f"Minimum length word: {min_word}")
 print(f"Maximum length word: {max_word}")
  
