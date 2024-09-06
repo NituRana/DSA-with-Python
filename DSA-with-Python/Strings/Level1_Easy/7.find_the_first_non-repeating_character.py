9ee096d18836c519c42b161d1255e2e62cea979f
NituRana <niturana0211@gmail.com>
2024-09-06 16:29:38 +0530

    revision of string

--- DSA-with-Python/Strings/Level1_Easy/7.find_the_first_non-repeating_character.py
+++ DSA-with-Python/Strings/Level1_Easy/7.find_the_first_non-repeating_character.py
@@ -6,17 +6,29 @@ Output : c
 
 
 ***********************************************************************************************************'''
-def first_non_repeating_char(s):
-    counts = {}
-    for char in s:
-        counts[char] = counts.get(char, 0) + 1
+# def first_non_repeating_char(s):
+#     counts = {}
+#     for char in s:
+#         counts[char] = counts.get(char, 0) + 1
     
-    for char in s:
-        if counts[char] == 1:
-            return char
-    return None
+#     for char in s:
+#         if counts[char] == 1:
+#             return char
+#     return None
 
-s = "aabbcdefg"
+# using dict 
+def first_non_repeating_char(string):
+    dict_of_char = {}
+    for char in string:
+        if char in dict_of_char:
+            dict_of_char[char] += 1
+        else:
+            dict_of_char[char] = 1
+    for char in string:
+        if dict_of_char[char] == 1:
+            return char 
+
+s = "aawbbcdefg"
 print(first_non_repeating_char(s))
- 
+
    
