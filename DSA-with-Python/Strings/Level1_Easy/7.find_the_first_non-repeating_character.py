6dcfb2be825841ac6f4815647cf3f3ec293277c2
NituRana <niturana0211@gmail.com>
2024-09-06 17:52:33 +0530

    brute force approach

--- DSA-with-Python/Strings/Level1_Easy/7.find_the_first_non-repeating_character.py
+++ DSA-with-Python/Strings/Level1_Easy/7.find_the_first_non-repeating_character.py
@@ -16,17 +16,30 @@ Output : c
 #             return char
 #     return None
 
-# using dict 
+
+# ------------ Bruce force approach ---------------
 def first_non_repeating_char(string):
-    dict_of_char = {}
-    for char in string:
-        if char in dict_of_char:
-            dict_of_char[char] += 1
-        else:
-            dict_of_char[char] = 1
-    for char in string:
-        if dict_of_char[char] == 1:
-            return char 
+    for i in range(len(string)):
+        is_char_dublicate = False
+        for j in range(len(string)):
+            if i != j and string[i] == string[j]:
+                is_char_dublicate = True
+                break
+        if is_char_dublicate == False:
+            return string[i]
+
+
+# ------------ using dict (optimal one) -----------
+# def first_non_repeating_char(string):
+#     dict_of_char = {}
+#     for char in string:
+#         if char in dict_of_char:
+#             dict_of_char[char] += 1
+#         else:
+#             dict_of_char[char] = 1
+#     for char in string:
+#         if dict_of_char[char] == 1:
+#             return char 
 
 s = "aawbbcdefg"
 print(first_non_repeating_char(s))
