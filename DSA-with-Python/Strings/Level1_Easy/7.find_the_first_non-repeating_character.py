2a2af1691b3b49225eefd7c49ba596b054ca2492
NituRana <niturana0211@gmail.com>
2024-09-09 11:29:31 +0530

    revision on frist non repeating char in string

--- DSA-with-Python/Strings/Level1_Easy/7.find_the_first_non-repeating_character.py
+++ DSA-with-Python/Strings/Level1_Easy/7.find_the_first_non-repeating_character.py
@@ -18,15 +18,15 @@ Output : c
 
 
 # ------------ Bruce force approach ---------------
-def first_non_repeating_char(string):
-    for i in range(len(string)):
-        is_char_dublicate = False
-        for j in range(len(string)):
-            if i != j and string[i] == string[j]:
-                is_char_dublicate = True
-                break
-        if is_char_dublicate == False:
-            return string[i]
+# def first_non_repeating_char(string):
+#     for i in range(len(string)):
+#         is_char_dublicate = False
+#         for j in range(len(string)):
+#             if i != j and string[i] == string[j]:
+#                 is_char_dublicate = True
+#                 break
+#         if is_char_dublicate == False:
+#             return string[i]
 
 
 # ------------ using dict (optimal one) -----------
@@ -40,6 +40,18 @@ def first_non_repeating_char(string):
 #     for char in string:
 #         if dict_of_char[char] == 1:
 #             return char 
+
+def first_non_repeating_char(string):
+    char_dict = {}
+    for i in string:
+        if i in char_dict:
+            char_dict[i] += 1
+        else:
+            char_dict[i] = 1
+    for char in string:
+        if char_dict[char] == 1:
+            return char
+    return -1
 
 s = "aawbbcdefg"
 print(first_non_repeating_char(s))
