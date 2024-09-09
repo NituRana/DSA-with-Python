d2458c1d1a3a32bf3981ded1add4ce9c2cd76549
NituRana <niturana0211@gmail.com>
2024-09-09 11:42:08 +0530

    reserve the all words in the string

--- DSA-with-Python/Strings/Level1_Easy/5.reverse_a_string.py
+++ DSA-with-Python/Strings/Level1_Easy/5.reverse_a_string.py
@@ -19,14 +19,24 @@ Output : sihT si a tset gnirts
 
 ***********************************************************************************************************'''
 
-def resersed_the_word(input_string):
-    words_list = input_string.split()
-    output_string = ""
-    for word in words_list:
-        reverse_word = word[::-1]
-        output_string += reverse_word + " "
-    return output_string
+# def resersed_the_word(input_string):
+    # words_list = input_string.split()
+    # output_string = ""
+    # for word in words_list:
+    #     reverse_word = word[::-1]
+    #     output_string += reverse_word + " "
+    # return output_string
 
+
+
+
+def resersed_the_word(input_string):
+    list_of_words = input_string.split()
+    output_string = ""
+    for current_word in list_of_words:
+        resersed_curr_word = current_word[::-1]
+        output_string += resersed_curr_word + " "
+    return output_string
 input_string = "This is a test string"
 print("----------------Input string :", input_string)
 res = resersed_the_word(input_string)
