78c5599af100e0179a2f3bb8cff16192db109854
NituRana <niturana0211@gmail.com>
2024-09-09 18:28:53 +0530

    revision of remove duplicate in the given string

--- DSA-with-Python/Strings/Level1_Easy/8.remove_all_duplicates_characters.py
+++ DSA-with-Python/Strings/Level1_Easy/8.remove_all_duplicates_characters.py
@@ -12,17 +12,37 @@ Output : c
 # s = "Hello here, I'm nitu rana"
 # print(removeDuplicates(s))
 
+# def removeDuplicates(s):
+#     counts = {}
+#     for c in s:
+#         counts[c] = counts.get(c, 0) + 1
+#     result = ''
+#     for c in s:
+#         if counts[c] == 1:
+#             result += c
+#     return result
+
+
 def removeDuplicates(s):
-    counts = {}
-    for c in s:
-        counts[c] = counts.get(c, 0) + 1
+    dict_of_char = {}
+    for i in s:
+        if i in dict_of_char:
+            dict_of_char[i] += 1
+        else:
+            dict_of_char[i] = 1
+    
     result = ''
-    for c in s:
-        if counts[c] == 1:
-            result += c
+    for i in s:
+        if dict_of_char[i] == 1:
+            result += i
+
     return result
 
+
+
+
 s = "Hello here, I'm nitu rana"
-print(removeDuplicates(s)) 
+res = removeDuplicates(s)
+print("-------------------- res : ", res)
 
    
