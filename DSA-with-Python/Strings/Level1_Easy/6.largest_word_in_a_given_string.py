0e5142e69109db1f68a8b7e8b37f37ba06ad964f
NituRana <niturana0211@gmail.com>
2024-09-09 11:34:18 +0530

    revision of largest word in the given string

--- DSA-with-Python/Strings/Level1_Easy/6.largest_word_in_a_given_string.py
+++ DSA-with-Python/Strings/Level1_Easy/6.largest_word_in_a_given_string.py
@@ -9,15 +9,23 @@ Output : structure
 #     words = s.split()
 #     return max(words, key=len)
 
+# def largest_word(string):
+#     words = string.split()
+#     max_word_len = 0
+#     largest_word = None
+#     for word in words:
+#         if len(word) > max_word_len:
+#             largest_word = word
+#             max_word_len = len(word)
+#     return largest_word
+
 def largest_word(string):
-    words = string.split()
-    max_word_len = 0
-    largest_word = None
-    for word in words:
-        if len(word) > max_word_len:
-            largest_word = word
-            max_word_len = len(word)
-    return largest_word
+    list_of_words = string.split()
+    max_word = ""
+    for current_word in list_of_words:
+        if len(max_word) < len(current_word):
+            max_word = current_word
+    return max_word
 
 
 s = "Welcome in my data structure repo"
