32caf4c55aa46f083faad8335e75730e20e59a74
NituRana <niturana0211@gmail.com>
2024-09-10 18:00:21 +0530

    revision of min, max element of array

--- DSA-with-Python/Arrays/Level1_Easy/2.Maximum_and_Minimum_Element_in_an_Array.py
+++ DSA-with-Python/Arrays/Level1_Easy/2.Maximum_and_Minimum_Element_in_an_Array.py
@@ -26,15 +26,33 @@ Input: [1, 7, 3, 6, 9, 15, 0, 2]
 Output: [0, 1, 2, 3, 6, 7, 9, 15]
 ***********************************************************************************************************'''
 
-def bubble_sort(arr):
-    for i in range(len(arr)):
-        for j in range(0, len(arr)-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-array = [1, 7, 3, 6, 9, 15, 0, 2]
-print(bubble_sort(array))
-print("this is the ascending array : ", array)
+# def bubble_sort(arr):
+#     for i in range(len(arr)):
+#         for j in range(0, len(arr)-i-1):
+#             if arr[j] > arr[j+1]:
+#                 arr[j], arr[j+1] = arr[j+1], arr[j]
+# array = [1, 7, 3, 6, 9, 15, 0, 2]
+# print(bubble_sort(array))
+# print("this is the ascending array : ", array)
 
 '''**********************************************************************************************************
 Bubble Sort - n^2
 ***********************************************************************************************************'''
+'''**********************************************************************************************************
+#--------------------------------------- revision ----------------------------------------------------
+
+
+***********************************************************************************************************'''
+input_array = [2, 5, 1, 8, 4]
+def max_min_element_in_array(input_array):
+    max_element = input_array[0]
+    min_element = input_array[0]
+    for i in input_array:
+        if min_element > i:
+            min_element = i
+        elif max_element < i:
+            max_element = i
+    return min_element, max_element
+min_ele, max_ele = max_min_element_in_array(input_array)
+print("------------------------ min element :", min_ele)
+print("------------------------ max element :", max_ele)
