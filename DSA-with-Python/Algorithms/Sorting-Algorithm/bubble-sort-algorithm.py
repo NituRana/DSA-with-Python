e83704b0fd389231a0b923bbfe9e554ae3e7b031
NituRana <niturana0211@gmail.com>
2024-09-11 12:30:33 +0530

    revise the bubble sort algo

--- DSA-with-Python/Algorithms/Sorting-Algorithm/bubble-sort-algorithm.py
+++ DSA-with-Python/Algorithms/Sorting-Algorithm/bubble-sort-algorithm.py
@@ -17,29 +17,37 @@ Explanation: After sorting we get 1,2,3,4,5
 
 '''
 
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n - i - 1):
-            if arr[j] > arr[j + 1]:
-                temp = arr[j +1]
-                arr[j + 1] = arr[j]
-                arr[j] = temp
-    return arr
-
-arr = [13,46,24,52,20,9]
-print("-------------- Before sorting :", arr)
-print("----- After sorting :", bubble_sort(arr))
 # def bubble_sort(arr):
 #     n = len(arr)
-#     for i in range(0, n-1):
-#         for j in range(0, n-i-1):
-#             if arr[j] > arr[j+1]:
-#                 # temp = arr[j+1]
-#                 # arr[j+1] = arr[j]
-#                 # arr[j] = temp
-#                 arr[j], arr[j+1] = arr[j+1], arr[j]
+#     for i in range(n):
+#         for j in range(0, n - i - 1):
+#             if arr[j] > arr[j + 1]:
+#                 temp = arr[j +1]
+#                 arr[j + 1] = arr[j]
+#                 arr[j] = temp
 #     return arr
 
 # arr = [13,46,24,52,20,9]
-# print(f"---- output : {bubble_sort(arr)}")
+# print("-------------- Before sorting :", arr)
+# print("----- After sorting :", bubble_sort(arr))
+
+
+
+
+
+
+
+
+def bubble_sort(unsort_array):
+    arr_len = len(unsort_array)
+    for i in range(0, arr_len-1):
+        for j in range(i, arr_len):
+            if unsort_array[i] > unsort_array[j]:
+                temp = unsort_array[i]
+                unsort_array[i] = unsort_array[j]
+                unsort_array[j] = temp
+    return unsort_array
+input_array = [7, 8, 4, 6, 0, 3, 24, 15, 9]
+print("--------------- input_array :", input_array)
+sorted_array = bubble_sort(input_array)
+print("--------------- sorted_array :", sorted_array)
