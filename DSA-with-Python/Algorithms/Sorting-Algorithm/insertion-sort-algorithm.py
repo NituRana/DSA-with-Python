22258c3e449fbdd4ad153569c68cc5ae387255d5
NituRana <niturana0211@gmail.com>
2024-09-15 22:48:41 +0530

    revise the insertion sorrt

--- DSA-with-Python/Algorithms/Sorting-Algorithm/insertion-sort-algorithm.py
+++ DSA-with-Python/Algorithms/Sorting-Algorithm/insertion-sort-algorithm.py
@@ -17,18 +17,31 @@ Output: 1,2,3,4,5
 Explanation: After sorting the array is: 1,2,3,4,5
 
 '''
+# def insertion_sort(arr):
+#     for i in range(1, len(arr)):
+#         key = arr[i]
+        
+#         j = i - 1
+#         while j >= 0 and key < arr[j]:
+#             arr[j + 1] = arr[j]
+#             j -= 1
+#         arr[j + 1] = key
+
+#     return arr
+
 def insertion_sort(arr):
     for i in range(1, len(arr)):
         key = arr[i]
-        
         j = i - 1
         while j >= 0 and key < arr[j]:
-            arr[j + 1] = arr[j]
+            arr[j+1] = arr[j]
             j -= 1
-        arr[j + 1] = key
-
+        arr[j+1] = key
     return arr
-arr = [12, 11, 13, 5, 6]
+
+
+
+arr = [12, 0, 11, 7, 13, 5, 6, 5, 13]
 print("Un-sort array:", arr)
 sorted_arr = insertion_sort(arr)
 print("Sorted array:", sorted_arr)
