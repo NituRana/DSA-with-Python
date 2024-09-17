f9f4d36b6965865c4c06ad76c570732263bd66c8
NituRana <niturana0211@gmail.com>
2024-09-17 22:37:45 +0530

    revise the satck using queue

--- DSA-with-Python/Stack/6.stack_using_a_queue.py
+++ DSA-with-Python/Stack/6.stack_using_a_queue.py
@@ -2,18 +2,32 @@
 stack using a queue:-
 
 ***********************************************************************************************************'''
+# class Stack:
+#     def __init__(self):
+#         self.queue = []
+    
+#     def push(self, item):
+#         self.queue.append(item)
+#         for i in range(len(self.queue)-1):
+#             self.queue.append(self.queue.pop(0))
+    
+#     def pop(self):
+#         if not self.queue:
+#             raise IndexError('Stack is empty')
+#         return self.queue.pop()
+
 class Stack:
     def __init__(self):
         self.queue = []
     
-    def push(self, item):
-        self.queue.append(item)
+    def push(self, data):
+        self.queue.append(data)
         for i in range(len(self.queue)-1):
             self.queue.append(self.queue.pop(0))
     
     def pop(self):
         if not self.queue:
-            raise IndexError('Stack is empty')
+            raise IndexError("Stack is empty!")
         return self.queue.pop()
 
 s = Stack()
