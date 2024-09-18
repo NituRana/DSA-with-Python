4441a0869bca684c0783e8227605ca6044c77c50
NituRana <niturana0211@gmail.com>
2024-09-18 16:45:44 +0530

    revise the insertion sort algo

--- DSA-with-Python/Algorithms/Sorting-Algorithm/insertion-sort-algorithm.py
+++ DSA-with-Python/Algorithms/Sorting-Algorithm/insertion-sort-algorithm.py
@@ -29,16 +29,16 @@ Explanation: After sorting the array is: 1,2,3,4,5
 
 #     return arr
 
+
 def insertion_sort(arr):
     for i in range(1, len(arr)):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and key < arr[j]:
+        current_min_ele = i
+        j = i-1
+        while j >= 0 and arr[j] > current_min_ele:
             arr[j+1] = arr[j]
             j -= 1
-        arr[j+1] = key
+        arr[j + 1] = current_min_ele
     return arr
-
 
 
 arr = [12, 0, 11, 7, 13, 5, 6, 5, 13]
