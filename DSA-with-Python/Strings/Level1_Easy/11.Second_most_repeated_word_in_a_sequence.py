5949099f11d5907899a18704ea34a0767c719ade
NituRana <niturana0211@gmail.com>
2024-09-26 17:48:26 +0530

    revise the second_most_repeated_word

--- DSA-with-Python/Strings/Level1_Easy/11.Second_most_repeated_word_in_a_sequence.py
+++ DSA-with-Python/Strings/Level1_Easy/11.Second_most_repeated_word_in_a_sequence.py
@@ -6,19 +6,32 @@ Output : l
 
 
 ***********************************************************************************************************'''
-def second_most_repeated_word(strings):
-    count = {}
-    for string in strings:
-        for word in string.split():
-            if word in count:
-                count[word] += 1
-            else:
-                count[word] = 1
-    sorted_count = sorted(count.items(), key=lambda x: x[1], reverse=True)
-    if len(sorted_count) < 2:
-        return None
-    else:
-        return sorted_count[1][0]
+# def second_most_repeated_word(strings):
+#     count = {}
+#     for string in strings:
+#         for word in string.split():
+#             if word in count:
+#                 count[word] += 1
+#             else:
+#                 count[word] = 1
+#     sorted_count = sorted(count.items(), key=lambda x: x[1], reverse=True)
+#     if len(sorted_count) < 2:
+#         return None
+#     else:
+#         return sorted_count[1][0]
+
+#-------------------------------- revise ----------------------
+
+def second_most_repeated_word(string):
+    stri = 'kjwde ejckc vvevemvev eveve'
+    char_dict = {}
+    for i in stri:
+        if i in char_dict:
+            char_dict[i] += 1
+        else:
+            char_dict[i] = 1
+    sorted_dict = dict(sorted(char_dict.items(), key=lambda item: item[1], reverse=True ))
+    return list(sorted_dict.keys())[0]
 
 # Example usage
 string = "Hello here, I'm nitu rana"
