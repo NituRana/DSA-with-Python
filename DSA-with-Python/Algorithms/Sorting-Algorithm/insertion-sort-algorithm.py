b309656369b78944d16070e4558d08760d706613
NituRana <niturana0211@gmail.com>
2024-10-04 14:21:58 +0530

    insertion sort

--- DSA-with-Python/Algorithms/Sorting-Algorithm/insertion-sort-algorithm.py
+++ DSA-with-Python/Algorithms/Sorting-Algorithm/insertion-sort-algorithm.py
@@ -40,16 +40,34 @@ Explanation: After sorting the array is: 1,2,3,4,5
 #         arr[j + 1] = current_min_ele
 #     return arr
 
+# def insertion_sort(arr):
+#     for i in range(1, len(arr)):
+#         last_sorted_ele_ind = i
+#         j = i-1
+#         while j >=0 and arr[last_sorted_ele_ind] > arr[j]:
+#             arr[j+1] = arr[j]
+#             j -= 1
+#         arr[j+1] = last_sorted_ele_ind
+#     return arr
+# arr = [12, 0, 11, 7, 13, 5, 6, 5, 13]
+# print("Un-sort array:", arr)
+# sorted_arr = insertion_sort(arr)
+# print("Sorted array:", sorted_arr)
+
+
+
+
 def insertion_sort(arr):
     for i in range(1, len(arr)):
-        last_sorted_ele_ind = i
+        min_ele = arr[i]
         j = i-1
-        while j >=0 and arr[last_sorted_ele_ind] > arr[j]:
+        while j >=0 and min_ele < arr[j]:
             arr[j+1] = arr[j]
-            j -= 1
-        arr[j+1] = last_sorted_ele_ind
+            j -=1
+        arr[j+1] = min_ele
     return arr
-arr = [12, 0, 11, 7, 13, 5, 6, 5, 13]
-print("Un-sort array:", arr)
-sorted_arr = insertion_sort(arr)
-print("Sorted array:", sorted_arr)
+
+array = [46, 28, 23, 15, 56, 90, 42, 12]
+print("--------------------- unsorted array :", array)
+sorted_array = insertion_sort(array)
+print("--------------------- sorted array :", sorted_array)
