7b48b0bddde847ac7777a209b58e637677919b1e
NituRana <niturana0211@gmail.com>
2024-10-07 23:48:08 +0530

    revise

--- DSA-with-Python/Algorithms/Sorting-Algorithm/insertion-sort-algorithm.py
+++ DSA-with-Python/Algorithms/Sorting-Algorithm/insertion-sort-algorithm.py
@@ -17,45 +17,6 @@ Output: 1,2,3,4,5
 Explanation: After sorting the array is: 1,2,3,4,5
 
 '''
-# def insertion_sort(arr):
-#     for i in range(1, len(arr)):
-#         key = arr[i]
-        
-#         j = i - 1
-#         while j >= 0 and key < arr[j]:
-#             arr[j + 1] = arr[j]
-#             j -= 1
-#         arr[j + 1] = key
-
-#     return arr
-
-
-# def insertion_sort(arr):
-#     for i in range(1, len(arr)):
-#         current_min_ele = i
-#         j = i-1
-#         while j >= 0 and arr[j] > current_min_ele:
-#             arr[j+1] = arr[j]
-#             j -= 1
-#         arr[j + 1] = current_min_ele
-#     return arr
-
-# def insertion_sort(arr):
-#     for i in range(1, len(arr)):
-#         last_sorted_ele_ind = i
-#         j = i-1
-#         while j >=0 and arr[last_sorted_ele_ind] > arr[j]:
-#             arr[j+1] = arr[j]
-#             j -= 1
-#         arr[j+1] = last_sorted_ele_ind
-#     return arr
-# arr = [12, 0, 11, 7, 13, 5, 6, 5, 13]
-# print("Un-sort array:", arr)
-# sorted_arr = insertion_sort(arr)
-# print("Sorted array:", sorted_arr)
-
-
-
 
 def insertion_sort(arr):
     for i in range(1, len(arr)):
