2e05e817a6702dbe559b6d500d5f9625d5a096b8
NituRana <niturana0211@gmail.com>
2024-10-08 15:57:31 +0530

    revise the string concept

--- DSA-with-Python/Strings/Level1_Easy/11.Second_most_repeated_word_in_a_sequence.py
+++ DSA-with-Python/Strings/Level1_Easy/11.Second_most_repeated_word_in_a_sequence.py
@@ -23,17 +23,17 @@ Output : l
 #-------------------------------- revise ----------------------
 
 def second_most_repeated_word(string):
-    stri = 'kjwde ejckc vvevemvev eveve'
+    # string = 'kwjwwwwwwwde ejckc vvevemvev eveve'
     char_dict = {}
-    for i in stri:
-        if i in char_dict:
-            char_dict[i] += 1
-        else:
-            char_dict[i] = 1
-    sorted_dict = dict(sorted(char_dict.items(), key=lambda item: item[1], reverse=True ))
-    return list(sorted_dict.keys())[0]
+    for i in string:
+        if i != ' ':
+            if i in char_dict:
+                char_dict[i] += 1
+            else:
+                char_dict[i] = 1
+    sorted_dict = dict(sorted(char_dict.items(), key=lambda item: item[1], reverse=True))
+    return list(sorted_dict.keys())[1]
 
-# Example usage
 string = "Hello here, I'm nitu rana"
 result = second_most_repeated_word(string)
 print(result)
