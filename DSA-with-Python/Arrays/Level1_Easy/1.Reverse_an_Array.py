7f0cf5c1fb5fbf83711d721242d3b8d3df3e1c63
NituRana <niturana0211@gmail.com>
2024-10-09 16:41:57 +0530

    revise the reverse the array elements

--- DSA-with-Python/Arrays/Level1_Easy/1.Reverse_an_Array.py
+++ DSA-with-Python/Arrays/Level1_Easy/1.Reverse_an_Array.py
@@ -10,23 +10,23 @@ Output: [2, 0, 15, 9, 6, 3, 7, 1]
 
 
 
-def reverse_arr(actual_arr):
-    reverse_arr = []
-    for i in range((len(actual_arr)-1), -1, -1):
-        reverse_arr.append(actual_arr[i])
-    return reverse_arr
+# def reverse_arr(actual_arr):
+#     reverse_arr = []
+#     for i in range((len(actual_arr)-1), -1, -1):
+#         reverse_arr.append(actual_arr[i])
+#     return reverse_arr
 
-print(reverse_arr([1, 7, 3, 6, 9, 15, 0, 2]))
+# print(reverse_arr([1, 7, 3, 6, 9, 15, 0, 2]))
 
 
 def reverse_the_arr(arr):
-    rev_arr = []
-    for i in range((len(arr) - 1), 0):
-        rev_arr.append(i)
-    return rev_arr
-array = [5, 8, 2, 4, 7, 9, 1]
-print(f"-------------------- input  :{array}-----")
-print("----------  reverse arr :", reverse_arr(array))
+    reverse_arr = []
+    for i in range((len(arr) - 1), -1, -1):
+        reverse_arr.append(arr[i])
+    return reverse_arr
+
+array = [45, 23, 46, 67, 89, 66, 32, 24]
+res = reverse_the_arr(array)
 
 '''****************************************
 Big O :- n
