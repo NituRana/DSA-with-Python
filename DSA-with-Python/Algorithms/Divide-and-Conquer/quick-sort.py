d4143758d7a49a13c953165c64fa5531d0c5d84e
NituRana <niturana0211@gmail.com>
2024-10-11 14:12:23 +0530

    revise the quick sort

--- DSA-with-Python/Algorithms/Divide-and-Conquer/quick-sort.py
+++ DSA-with-Python/Algorithms/Divide-and-Conquer/quick-sort.py
@@ -2,30 +2,50 @@
 #------------------------  approche : In-place algo --------------------
 #-----------------------------------------------------------------------
 
-def quicksort(arr, low, high):
+# def quicksort(arr, low, high):
+#     if low < high:
+#         partition_ele = partition(arr, low, high)
+#         quicksort(arr, low, partition_ele - 1) #left side element
+#         quicksort(arr, partition_ele + 1, high)
+
+# def partition(arr, low, high):
+#     pivot_ele = arr[high]
+#     i = low - 1
+#     for j in range(low, high):
+#         if arr[j] <= pivot_ele:
+#             # Increment the index of smaller elements
+#             i += 1
+#             # Swap current element with the element at index 'i'
+#             arr[i], arr[j] = arr[j], arr[i]
+#     # Place the pivot element in its correct position (after all smaller elements)
+#     arr[i+1], arr[high] = arr[high], arr[i+1]
+#     return i+1
+
+def quick_sort(arr, low, high):
     if low < high:
         partition_ele = partition(arr, low, high)
-        quicksort(arr, low, partition_ele - 1) #left side element
-        quicksort(arr, partition_ele + 1, high)
+        left_side = quick_sort(arr, low, partition_ele-1)
+        right_side = quick_sort(arr, partition_ele+1, high)
 
 def partition(arr, low, high):
-    pivot_ele = arr[high]
-    i = low - 1
+    pivot  = arr[high]
+    i = low -1
     for j in range(low, high):
-        if arr[j] <= pivot_ele:
-            # Increment the index of smaller elements
+        if arr[j] <= pivot:
             i += 1
-            # Swap current element with the element at index 'i'
             arr[i], arr[j] = arr[j], arr[i]
-    # Place the pivot element in its correct position (after all smaller elements)
     arr[i+1], arr[high] = arr[high], arr[i+1]
     return i+1
 
+array = [34, 66, 23, 12, 35, 5, 67, 79, 42, 50, 85]
+print("------------------- unsort array :", array)
+quick_sort(array, 0, len(array)-1)
+print("------------------- sort array :", array)
 
 
-arr = [33, 10, 59, 26, 41, 78, 14, 55]
-quicksort(arr, 0, len(arr) - 1)
-print("------------- sorted array :", arr)
+# arr = [33, 10, 59, 26, 41, 78, 14, 55]
+# quicksort(arr, 0, len(arr) - 1)
+# print("------------- sorted array :", arr)
 
 #-----------------------------------------------------------------------
 #--------------------- Second approche : Non in-place algo--------------
