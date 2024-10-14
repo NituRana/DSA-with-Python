8d7003c4211ad94c60a788514f146c601f35e234
NituRana <niturana0211@gmail.com>
2024-10-14 12:45:54 +0530

    revise the merge sort

--- DSA-with-Python/Algorithms/Divide-and-Conquer/merge-sort-algorithm.py
+++ DSA-with-Python/Algorithms/Divide-and-Conquer/merge-sort-algorithm.py
@@ -53,34 +53,57 @@ Output: 1,2,3,4,5,8,23
 
 
 
+# def merge_sort(unsort_arr):
+#     if len(unsort_arr)<= 1:
+#         return unsort_arr
+
+#     mid = len(unsort_arr) // 2
+#     left_arr = unsort_arr[:mid]
+#     right_arr = unsort_arr[mid:]
+
+#     sorted_left = merge_sort(left_arr)
+#     sorted_right = merge_sort(right_arr)
+
+#     return merge(sorted_left, sorted_right)
+
+# def merge(left_arr, right_arr):
+#     sorted_arr = []
+#     i = j = 0
+#     while i < len(left_arr) and j < len(right_arr):
+#         if left_arr[i] < right_arr[j]:
+#             sorted_arr.append(left_arr[i])
+#             i += 1
+#         else:
+#             sorted_arr.append(right_arr[j])
+#             j += 1
+#     sorted_arr.extend(left_arr[i:])
+#     sorted_arr.extend(right_arr[j:])
+#     return sorted_arr
+
+
 def merge_sort(unsort_arr):
-    if len(unsort_arr)<= 1:
+    if len(unsort_arr)==1:
         return unsort_arr
-
     mid = len(unsort_arr) // 2
-    left_arr = unsort_arr[:mid]
-    right_arr = unsort_arr[mid:]
-
-    sorted_left = merge_sort(left_arr)
-    sorted_right = merge_sort(right_arr)
-
-    return merge(sorted_left, sorted_right)
+    left_arr = merge_sort(unsort_arr[:mid])
+    right_arr = merge_sort(unsort_arr[mid:])
+    return merge(left_arr, right_arr)
 
 def merge(left_arr, right_arr):
-    sorted_arr = []
+    res_arr = []
     i = j = 0
     while i < len(left_arr) and j < len(right_arr):
-        if left_arr[i] < right_arr[j]:
-            sorted_arr.append(left_arr[i])
-            i += 1
-        else:
-            sorted_arr.append(right_arr[j])
+        if left_arr[i] > right_arr[j]:
+            res_arr.append(right_arr[j])
             j += 1
-    sorted_arr.extend(left_arr[i:])
-    sorted_arr.extend(right_arr[j:])
-    return sorted_arr
-
+        else:
+            res_arr.append(left_arr[i])
+            i += 1
+    res_arr.extend(left_arr[i:])
+    res_arr.extend(right_arr[j:])
+    return res_arr
 
 arr = [13, 46, 24, 52, 20, 9]
+print(f"---- input  : {arr}")
 sorted_arr = merge_sort(arr)
 print(f"---- output : {sorted_arr}")
