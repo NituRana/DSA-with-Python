524afb4a3965417e6cf47f05b9cc22b863133ad5
NituRana <niturana0211@gmail.com>
2024-11-03 23:39:02 +0530

    string question

--- DSA-with-Python/Strings/Level1_Easy/6.largest_word_in_a_given_string.py
+++ DSA-with-Python/Strings/Level1_Easy/6.largest_word_in_a_given_string.py
@@ -5,19 +5,6 @@ Output : structure
 
 
 ***********************************************************************************************************'''
-# def largest_word(s):
-#     words = s.split()
-#     return max(words, key=len)
-
-# def largest_word(string):
-#     words = string.split()
-#     max_word_len = 0
-#     largest_word = None
-#     for word in words:
-#         if len(word) > max_word_len:
-#             largest_word = word
-#             max_word_len = len(word)
-#     return largest_word
 
 def largest_word(string):
     list_of_word = string.split()
