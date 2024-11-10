55b1825cb74ffed9f75a34eb59366bd2f04b1fc8
NituRana <niturana0211@gmail.com>
2024-11-10 23:24:21 +0530

    revise the string concept

--- DSA-with-Python/Strings/Level2_medium/2.Find_word_location_in_String.py
+++ DSA-with-Python/Strings/Level2_medium/2.Find_word_location_in_String.py
@@ -1,18 +1,19 @@
 '''*******************************************************************************************************
 Find Word location in String in the given string.
 
-Input : "hello here I am nitu rana"
-Output : I
+Input : "Revising The String Concepts"
+Output : C
 
 
 ***********************************************************************************************************'''
-input_str = '"hello here I am nitu rana"'
+
+input_str = '"Revising The String Concepts"'
  
 print("The original string is : " + input_str)
  
-wrd = 'I'
+word = 'c'
  
 x=input_str.split()
 print(x)
-res=x.index(wrd)+1
+res=x.index(word)+1
 print("The location of word is : " + str(res))
