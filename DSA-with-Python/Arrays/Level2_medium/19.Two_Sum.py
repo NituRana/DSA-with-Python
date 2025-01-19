a79b35691a97d99731acf55b6217f56384a9ca7d
NituBfit <nitu.rana@spectacom.in>
2025-01-19 23:42:07 +0530

    tow sum revison

--- DSA-with-Python/Arrays/Level2_medium/19.Two_Sum.py
+++ DSA-with-Python/Arrays/Level2_medium/19.Two_Sum.py
@@ -14,50 +14,61 @@ Result: NO (for 1st variant)
 Explanation: There exist no such two numbers whose sum is equal to the target.
 
 ***********************************************************************************************************'''
-#1. Brute-force approach
-def two_sum(n, arr, target):
-    for i in range(n):
-        for j in range(i + 1, n):
-            if arr[i] + arr[j] == target:
-                return "YES"
-    return "NO"
+# #1. Brute-force approach
+# def two_sum(n, arr, target):
+#     for i in range(n):
+#         for j in range(i + 1, n):
+#             if arr[i] + arr[j] == target:
+#                 return "YES"
+#     return "NO"
 
-def main():
-    n = 5
-    arr = [2, 6, 5, 8, 11]
-    target = 8
-    ans = two_sum(n, arr, target)
-    print("This is the answer for variant 1:", ans)
+# def main():
+#     n = 5
+#     arr = [2, 6, 5, 8, 11]
+#     target = 8
+#     ans = two_sum(n, arr, target)
+#     print("This is the answer for variant 1:", ans)
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
 
 #2. 
 
 
 #3. Optimized Approach(using two-pointer): 
 
-def two_sum(n, arr, target):
-    arr.sort()
-    left = 0
-    right = n - 1
-    while left < right:
-        total = arr[left] + arr[right]
-        if total == target:
-            return "YES"
-        elif total < target:
-            left += 1
-        else:
-            right -= 1
+# def two_sum(n, arr, target):
+#     arr.sort()
+#     left = 0
+#     right = n - 1
+#     while left < right:
+#         total = arr[left] + arr[right]
+#         if total == target:
+#             return "YES"
+#         elif total < target:
+#             left += 1
+#         else:
+#             right -= 1
+#     return "NO"
+
+# def main():
+#     n = 5
+#     arr = [2, 6, 5, 8, 11]
+#     target = 6
+#     ans = two_sum(n, arr, target)
+#     print("This is the answer for variant 1:", ans)
+
+# if __name__ == "__main__":
+#     main()
+
+a = [5, 7, 3, 2, 8, 9, 4]
+target = 15
+
+def tow_sum(a, target):
+    for i in range(0, len(a)-1):
+        for j in range(i+1, len(a)-1):
+            if a[i] + a[j] == target:
+                return "YES"
     return "NO"
 
-def main():
-    n = 5
-    arr = [2, 6, 5, 8, 11]
-    target = 6
-    ans = two_sum(n, arr, target)
-    print("This is the answer for variant 1:", ans)
-
-if __name__ == "__main__":
-    main()
-    
+print("----- Tow Sum revison :", tow_sum(a, target))
