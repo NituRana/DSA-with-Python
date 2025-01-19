653d06901979076cd2cb2de7b04cd4dd6477e00b
NituBfit <nitu.rana@spectacom.in>
2025-01-19 23:45:22 +0530

    add all in main

---
+++ DSA-with-Python/Arrays/Level2_medium/16.Find_the_missing_number_in_an_array.py
@@ -0,0 +1,98 @@
+'''*************************************************************************************************************************
+Problem Statement: Given an integer N and an array of size N-1 containing N-1 numbers between 1 to N. Find the number
+(between 1 to N), that is not present in the given array.
+
+Example 1:
+Input Format:
+ N = 5, array[] = {1,2,4,5}
+Result:
+ 3
+Explanation: 
+In the given array, number 3 is missing. So, 3 is the answer.
+
+Example 2:
+Input Format:
+ N = 3, array[] = {1,3}
+Result:
+ 2
+Explanation: 
+In the given array, number 2 is missing. So, 2 is the answer.
+************************************************************************************************************************'''
+
+# 1 :  Brute Force Approach
+
+def missing_number(a, N):
+    # Outer loop that runs from 1 to N:
+    for i in range(1, N + 1):
+        # flag variable to check if an element exists
+        flag = 0
+
+        # Search the element using linear search:
+        for j in range(len(a)):
+            if a[j] == i:
+                # i is present in the array:
+                flag = 1
+                break
+
+        # check if the element is missing (i.e., flag == 0):
+        if flag == 0:
+            return i
+
+    # The following line will never execute.
+    # It is just to avoid warnings.
+    return -1
+
+def main():
+    N = 5
+    a = [1, 2, 4, 5]
+    ans = missing_number(a, N)
+    print("The missing number is:", ans)
+
+if __name__ == '__main__':
+    main()
+
+'''**********************************************************************************************************
+2. :
+
+def missingNumber(a, N):
+    # Summation of first N numbers:
+    summation = (N * (N + 1)) // 2
+
+    # Summation of all array elements:
+    s2 = sum(a)
+
+    missingNum = summation - s2
+    return missingNum
+
+def main():
+    N = 5
+    a = [1, 2, 4, 5]
+    ans = missingNumber(a, N)
+    print("The missing number is:", ans)
+
+if __name__ == '__main__':
+    main()
+
+def missingNumber(a, N):
+    xor1 = 0
+    xor2 = 0
+
+    for i in range(N - 1):
+        xor2 = xor2 ^ a[i]  # XOR of array elements
+        xor1 = xor1 ^ (i + 1)  # XOR up to [1...N-1]
+    
+    xor1 = xor1 ^ N  # XOR up to [1...N]
+
+    return xor1 ^ xor2  # the missing number
+
+
+def main():
+    N = 5
+    a = [1, 2, 4, 5]
+    ans = missingNumber(a, N)
+    print("The missing number is:", ans)
+
+
+if __name__ == '__main__':
+    main()
+***********************************************************************************************************'''
